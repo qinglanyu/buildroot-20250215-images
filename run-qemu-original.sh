6db0e24df591c4ea9400a738d20e37d8b6d78426
qinglanyu <qinglanyu_jun@foxmail.com>
2026-09-13 08:07:53 +0800

    更新设备树文件禁用MMDC,OCOTP,LCDIF,Panel,USB1,USB2,USBMISC,CAAM,QSPI；扩展rootfs.ext2到64MB; 新增2个脚本更新设备树及启动qemu

---
+++ run-qemu-original.sh
@@ -0,0 +1,10 @@
+qemu-system-arm \
+        -M mcimx6ul-evk \
+        -m 512M \
+        -kernel zImage \
+        -dtb imx6ul-14x14-evk-qemu.dtb \
+        -drive file=rootfs.ext2,format=raw,if=sd \
+        -append "console=ttymxc0 root=/dev/mmcblk0 rw rootwait" \
+        -nographic \
+        -no-reboot
+
---
+++ update-dtb.sh
@@ -0,0 +1,1 @@
+dtc -I dts -O dtb -o imx6ul-14x14-evk-qemu.dtb imx6ul-14x14-evk-qemu.dts
